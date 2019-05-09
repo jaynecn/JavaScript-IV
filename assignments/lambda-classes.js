f24931378df6b013b8bbdc68f54e639fa56b55f8
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-05-09 14:26:10 +0100

    planned classes, now starting to code it

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class Person {
+  constuctor(personInfo) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+    this.gender = gender;
+  }
+
+  speak() {
+    console.log(`Hello, my name is ${this.name} I am from ${this.location}`);
+  }
+}
