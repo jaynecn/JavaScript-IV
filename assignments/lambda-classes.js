657cec5b2e3dd7562c1c0e571723a6d1f1fe8376
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-05-09 14:45:39 +0100

    Person and Instructor working, moving on to Student

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,13 +1,40 @@
 // CODE here for your Lambda Classes
 class Person {
-  constuctor(personInfo) {
-    this.name = name;
-    this.age = age;
-    this.location = location;
-    this.gender = gender;
+  constructor(personInfo) {
+    this.name = personInfo.name;
+    this.age = personInfo.age;
+    this.location = personInfo.location;
+    this.gender = personInfo.gender;
   }
-
   speak() {
     console.log(`Hello, my name is ${this.name} I am from ${this.location}`);
   }
 }
+
+class Instructor extends Person {
+  constructor(instructorInfo) {
+    super(instructorInfo);
+    this.specialty = instructorInfo.specialty;
+    this.favLanguage = instructorInfo.favLanguage;
+    this.catchPhrase = instructorInfo.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
+}
+
+
+
+
+
+
+const matt = new Person ({name: 'Matt', age:42, location: 'Aberdeen', gender: 'M'});
+matt.speak();
+
+const gabe = new Instructor ({name: 'Gabriel', age:22, location: 'New York', gender: 'M', specialty: 'teaching', favLanguage: 'Javascript', catchPhrase: 'Propaganda'})
+gabe.speak();
+gabe.demo('debugging');
+gabe.grade({name: 'Martin', age: 23}, 'helping classmates');
